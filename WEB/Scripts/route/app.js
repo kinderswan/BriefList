4895df5590619627223cf1224b3d1b1586874204
kinderswan <gamanovichpavel@gmail.com>
2016-03-01 15:57:20 +0300

    Conflict resolving commit

--- WEB/Scripts/route/app.js
+++
@@ -1,9 +0,0 @@
-﻿var myApp = angular.module('myApp', ["ngRoute"])
-    .config(function($routeProvider){
-        $routeProvider.when('showLists',
-        {
-            templateUrl:'api/users/2/lists',
-            controller:'ListController'
-        });
-
-})
