d16b5dbeaa16e112619204b52ccd64b93a3efd28
kinderswan <gamanovichpavel@gmail.com>
2016-03-01 15:03:30 +0300

    Create HttpGetResponse helper

--- WEB/Controllers/Api/UsersController.cs
+++ WEB/Controllers/Api/UsersController.cs
@@ -4,9 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
+using BLL.Interfaces.BLLModels;
 using BLL.Interfaces.Interfaces;
+using Newtonsoft.Json;
+using WEB.HelperExtension;
 using WEB.Mapping;
 using WEB.Models.ApiModels;
 
@@ -27,47 +33,45 @@ namespace WEB.Controllers.Api
             _itemService = itemService;
         }
 
+
         #region get
         [HttpGet]
-        [Route("api/users")]
-        public async Task<IEnumerable<ApiUserProfile>> GetUsers()
+        public async Task<HttpResponseMessage> GetUsers()
         {
-            return (await _userService.GetUserProfiles()).Select(Mapper.ToApiUserProfile);
+            var helper = new HttpResponseGetHelper<IEnumerable<ApiUserProfile>>(User.Identity.IsAuthenticated, Request);
+            helper.Method( async ()=> (await _userService.GetUserProfiles()).Select(Mapper.ToApiUserProfile));
+            return await helper.Result();
         }
 
         [HttpGet]
-        [Route("api/users/{id}")]
-        public async Task<ApiUserProfile> GetUser(int id)
+        public async Task<HttpResponseMessage> GetUser(int id)
         {
-            return Mapper.ToApiUserProfile(await _userService.GetUserProfile(id));
+            var helper = new HttpResponseGetHelper<ApiUserProfile>(User.Identity.IsAuthenticated, Request);
+            helper.Method( async () => Mapper.ToApiUserProfile(await _userService.GetUserProfile(id)));
+            return await helper.Result();
         }
 
         [HttpGet]
-        [Route("api/users/{name}")]
-        public async Task<ApiUserProfile> GetUser(string name)
+        public async Task<HttpResponseMessage> GetUser(string name)
         {
-            return Mapper.ToApiUserProfile(await _userService.GetUserProfile(name));
+            var helper = new HttpResponseGetHelper<ApiUserProfile>(User.Identity.IsAuthenticated, Request);
+            helper.Method(async () => Mapper.ToApiUserProfile(await _userService.GetUserProfile(name)));
+            return await helper.Result();
         }
 
-        
-        
 
-        
+
+
+
         //todo: add /id to get controllers
         #endregion
 
         #region delete
 
-        [HttpDelete]
-        public void DeleteUser(int id)
-        {
-            _userService.DeleteUserProfile(id);
-        }
-
         #endregion
 
         #region post
-        
+
         #endregion
     }
 }
